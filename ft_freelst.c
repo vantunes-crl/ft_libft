d29a7ff90fa1f6edb8e22002cece154c3c7735d1
Valerio Antunes de souza junior <vantunes@c1r17s15.42lisboa.com>
2021-07-28 17:25:44 +0100

    norm

--- ft_freelst.c
+++ ft_freelst.c
@@ -1,14 +1,14 @@
 #include "libft.h"
 
-void ft_freelst(t_list *elem)
+void	ft_freelst(t_list *elem)
 {
-    t_list *temp;
+	t_list	*temp;
 
-    while (elem)
-    {
-        temp = elem;
-        elem = elem->next;
-        free(temp->content);
-        free(temp);
-    }
+	while (elem)
+	{
+		temp = elem;
+		elem = elem->next;
+		free(temp->content);
+		free(temp);
+	}
 }
